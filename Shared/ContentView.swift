006fbc9ec3c8607748702639b5b9a3e4a557c4f4
Raphael Martin <Raphael.Martin.-ND@disney.com>
2022-08-16 09:59:01 -0300

    Created first layout definitions

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 15)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("São Paulo")
+                }
+            }
+        }
     }
 }
 
