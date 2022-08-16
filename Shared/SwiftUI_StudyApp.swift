066d74a12e16f0fb761cf65d1dec6439458383a2
Raphael Martin <Raphael.Martin.-ND@disney.com>
2022-08-16 09:10:30 -0300

    Initial Commit

---
+++ Shared/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  Shared
+//
+//  Created by Raphael Martin on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
