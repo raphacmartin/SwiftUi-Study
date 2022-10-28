108150fa7a2389878454848cfff627b1dcf87796
Raphael Martin <raphaelcastromartin@gmail.com>
2022-10-28 12:15:56 -0300

    Added an example unit test

---
+++ Unit Tests/StubTests.swift
@@ -0,0 +1,14 @@
+//
+//  StubTests.swift
+//  Tests iOS
+//
+//  Created by Rapha on 10/28/22.
+//
+
+import XCTest
+
+class StubTests: XCTestCase {
+    func testThatPasses() throws {
+        XCTAssert(true)
+    }
+}
